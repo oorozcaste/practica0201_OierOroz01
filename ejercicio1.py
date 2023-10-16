d75365964574636f71a3702371d069005653c6e8
oorozcaste <145026396+oorozcaste@users.noreply.github.com>
2023-10-16 19:33:27 +0200

    m

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,3 @@
-n = 0
 nombre = input("introduzca nombre de usuario: ")
-n = int(input("introduzca un numero entero"))
-while  n > 0:
- print(nombre)
- n -= 1
+n = int(input("introduzca un numero entero: "))
+print((nombre + "\n" )*n)
